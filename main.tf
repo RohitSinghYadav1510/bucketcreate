5c22aeb7b72a68bfd242ff67f0479b3e05978741
Rohit Singh yadav <yadavrohit1510@gmail.com>
2021-09-16 18:10:42 +0530

    sdssd

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "aws_kms_key" "kmskey" {
-  description             = var.bucketkey
-  deletion_window_in_days = 10
-}
+
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket
@@ -12,15 +9,4 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = true
   }
-
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.kmskey.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
 }
